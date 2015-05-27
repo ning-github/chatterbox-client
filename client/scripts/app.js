c38ae2e52eeaae2bddcb14f5ffa2eff27bd2aafb
ning-github <nxiamail@gmail.com>
2015-05-26 19:59:58 -0700

    refactor code to use Backbone.js framework

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,4 +1,95 @@
-// YOUR CODE HERE:
+// /////////////////////////////////////////
+// BACKBONE.JS IMPLEMENTATION
+// /////////////////////////////////////////
+var Message = Backbone.Model.extend({
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  defaults: {
+    username: ''
+  }
+});
+
+var Messages = Backbone.Collection.extend({
+  model: Message,
+  url: 'https://api.parse.com/1/classes/chatterbox',
+
+  loadMessages: function () {
+    this.fetch({data: {order: '-createdAt'}});
+  },
+
+  parse: function (response, options) {
+    var output = [];
+
+    for (var i = response.results.length-1; i>=0; i--) {
+      output.push(response.results[i]);
+    }
+
+    return output;
+  }
+});
+
+var MessageView = Backbone.View.extend({
+  model: Message,
+
+  template: _.template(['<li class="well">',
+        '<div class="username"><%-username%></div>',
+        '<div class="msgText"><%-text%></div>',
+        '<div><%-roomname%></div>',
+      '</li>',].join('')),
+
+  render: function () {
+    this.$el.html(this.template(this.model.attributes));
+    return this.$el;
+  }
+});
+
+var MessagesView = Backbone.View.extend({
+  initialize: function () {
+    this.collection.on('sync', this.render, this);
+    this.onScreenMessages = {};
+  },
+
+  render: function () {
+    this.collection.forEach(this.renderMessage, this);
+  },
+
+  renderMessage: function(item){
+    // if it's not currently on screen
+    if (!this.onScreenMessages[item.get('objectId')]){
+      this.onScreenMessages[item.get('objectId')] = true;
+      var newMessageView = new MessageView({model:item});
+      this.$el.prepend(newMessageView.render());
+    }
+  }
+});
+
+var SubmitView = Backbone.View.extend({
+
+  events: {
+    'click .submit': 'doSubmit'
+  },
+
+  doSubmit: function(e){
+    e.preventDefault();
+
+    var $text = this.$('#message');
+    var $roomname = this.$('#roomname');
+    this.collection.create({
+      username: window.location.search.substr(10),
+      text: $text.val(),
+      roomname: $roomname.val()
+    });
+    $text.val('');
+    $roomname.val('');
+  },
+
+});
+
+
+// /////////////////////////////////////////
+// ORIGINAL JQUERY IMPLEMENTATION
+// /////////////////////////////////////////
+
+
 // /////////////////////////////////////////
 // chatterbox Application Class
 // /////////////////////////////////////////
@@ -131,58 +222,58 @@ App.prototype.init = function(){
   });
 };
 
-var app = new App();
-app.init();
+// var app = new App();
+// app.init();
 
 /////////////////////////////////////////////////
 /// jQuery DOM interactions
 /////////////////////////////////////////////////
-$(document).on('ready', function () {
+// $(document).on('ready', function () {
 
-  // begin auto feed-refresh
-  $('.menu').on('click', 'button.refresh', function(e) {
-    app.refreshID = setInterval(app.refreshMessages.bind(app), 1000);
-    $(this).text('Stop Refreshing');
-    $(this).toggleClass('stopRefresh');
-    $(this).toggleClass('refresh');
-  });
+//   // begin auto feed-refresh
+//   $('.menu').on('click', 'button.refresh', function(e) {
+//     app.refreshID = setInterval(app.refreshMessages.bind(app), 1000);
+//     $(this).text('Stop Refreshing');
+//     $(this).toggleClass('stopRefresh');
+//     $(this).toggleClass('refresh');
+//   });
 
-  // stop auto feed-refresh
-  $('.menu').on('click', 'button.stopRefresh', function(e) {
-    if (app.refreshID) {
-      clearInterval(app.refreshID);
-    }
-    $(this).text('Refresh');
-    $(this).toggleClass('stopRefresh');
-    $(this).toggleClass('refresh');
-  });
+//   // stop auto feed-refresh
+//   $('.menu').on('click', 'button.stopRefresh', function(e) {
+//     if (app.refreshID) {
+//       clearInterval(app.refreshID);
+//     }
+//     $(this).text('Refresh');
+//     $(this).toggleClass('stopRefresh');
+//     $(this).toggleClass('refresh');
+//   });
 
-  // submit a new message/room
-  $('.submit').on('click', function(e){
-    var room = $('#roomname').val();
+//   // submit a new message/room
+//   $('.submit').on('click', function(e){
+    // var room = $('#roomname').val();
 
-    if (room === '') {
-      room = $('#roomSelect').val();
-    }
+    // if (room === '') {
+    //   room = $('#roomSelect').val();
+    // }
 
-    var msg = {
-      username: app.username,
-      roomname: room,
-      text: $('#message').val()
-    };
+    // var msg = {
+    //   username: app.username,
+    //   roomname: room,
+    //   text: $('#message').val()
+    // };
 
-    app.send(msg);
-    $('#message').val('');
-  });
+    // app.send(msg);
+//     $('#message').val('');
+//   });
 
-  // add a friend
-  $('#main').on('click', '.username', function (e) {
-    var name = $(this).text();
-    if (!(name in app.friends)) {
-      $('.friends-list').append($('<li></li>').text(name));
-      app.friends[name] = name;
-    }
-    app.highlightFriends();
-  });
+//   // add a friend
+//   $('#main').on('click', '.username', function (e) {
+//     var name = $(this).text();
+//     if (!(name in app.friends)) {
+//       $('.friends-list').append($('<li></li>').text(name));
+//       app.friends[name] = name;
+//     }
+//     app.highlightFriends();
+//   });
 
-});
+// });
